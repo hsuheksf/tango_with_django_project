c88aed8bf59f48c2e5a50eba09834249048b3c6b
Haowen Li <2327962l@student.gla.ac.uk>
2019-02-07 06:24:37 +0800

    bug fixed 2

--- rango/models.py
+++ rango/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 	name = models.CharField(max_length=max_name_length, unique=True)
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
-	slug = models.SlugField()
+	slug = models.SlugField(unique=True)
 
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.name)
@@ -25,6 +25,9 @@ class Page(models.Model):
 	title = models.CharField(max_length=max_title_length)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
+
+	class Meta:
+		verbose_name_plural = 'Pages'
 
 	def __str__(self): # For Python 2, use __unicode__ too
 		return self.title
